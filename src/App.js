85fd64feed17e79666b75322aa6eb2133b03bddc
Tejaaswini <tejnaren07@gmail.com>
2021-08-09 08:56:06 +0530

    update landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Lottie from '../node_modules/react-lottie';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import animationData from './Assets/lottie/coming_soon.json';
+
+
 
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <Lottie options={defaultOptions}
+                  className="lottie"
+                  height={800}
+                  width={1500}
+                />
+        <h3 style={{marginTop:'5vh'}}>
+          <a href="https://github.com/Tejaaswini">Tejaaswini Narendra </a>
+        </h3>
+      </div>
+    </Router>
   );
 }
 
